ceeb5e25a30adcaaed2b7e38c3634fe57fd28aa5
yoptohlejepeta <peta.kotlan@seznam.cz>
2023-09-11 21:51:53 +0200

    404 handler, home route

--- app/__main__.py
+++ app/__main__.py
@@ -1,25 +1,33 @@
-from fastapi import FastAPI, Request, Cookie
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 from dotenv import load_dotenv
 import os
+
+from routers import home
 
 load_dotenv()
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
+@app.exception_handler(404)
+async def handler_404(request: Request, exc: HTTPException):
+    return templates.TemplateResponse("404.html", {"request": request})
+
+
 @app.get("/")
 def login(request: Request, stagUserTicket: str = None):
     login_url = os.getenv("LOGIN_URL")
     if stagUserTicket:
-        return templates.TemplateResponse(
-            "home.html", {"request": request, "ticket": stagUserTicket}
-        )
+        return RedirectResponse(url=f"/home/?stagUserTicket={stagUserTicket}")
     else:
         return templates.TemplateResponse(
             "login.html", {"request": request, "login_url": login_url}
         )
 
+
+app.include_router(home.router)
 
 if __name__ == "__main__":
     import uvicorn
---
+++ app/routers/home.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
+
+router = APIRouter(prefix="/home", tags=["Home page"])
+
+
+@router.get("/")
+async def home(request: Request, stagUserTicket: str = None):
+    return templates.TemplateResponse(
+        "home.html", {"request": request, "ticket": stagUserTicket}
+    )
