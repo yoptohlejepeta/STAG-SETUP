18fa0274000faad01584896cc7a677877af3b269
yoptohlejepeta <peta.kotlan@seznam.cz>
2023-09-11 20:51:02 +0200

    basic login

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # STAG-Setup
 
-Příprava pro aplikace, které potřebují přihlášení přes STAG.
+Příprava pro aplikace, které potřebují přihlášení přes [Ujep STAG](https://portal.ujep.cz/).
 
 * [Fastapi](https://fastapi.tiangolo.com/)
 * [Pico.css](https://picocss.com/)
+
+## Deployment
+
+1. Vytvořte soubor **.env** (podle vzoru **.env_example**), včetně proměnné **LOGIN_URL**
+2. Změňte **origin_url** parametr na URL stránky, na kterou budete chtít vrátit stagUserTicket
+
+## TODO
+
+* Session
--- app/__main__.py
+++ app/__main__.py
@@ -1,20 +1,24 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Cookie
 from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
+import os
 
-
+load_dotenv()
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-# @app.get("/favicon.ico")
-# async def icon():
-#     return FileResponse("static/stag_favcon.ico")
+def login(request: Request, stagUserTicket: str = None):
+    login_url = os.getenv("LOGIN_URL")
+    if stagUserTicket:
+        return templates.TemplateResponse(
+            "home.html", {"request": request, "ticket": stagUserTicket}
+        )
+    else:
+        return templates.TemplateResponse(
+            "login.html", {"request": request, "login_url": login_url}
+        )
 
 
 if __name__ == "__main__":
